643c327093faa9be48e776e4f69b2c27e4dbe747
Rodrigo Camargo <camargo@morphy.com.br>
2020-10-17 23:23:09 -0300

    many

---
+++ utils.js
@@ -0,0 +1,10 @@
+this.pad_with_zeroes = function(number, length) {
+
+    var my_string = '' + number;
+    while (my_string.length < length) {
+        my_string = '0' + my_string;
+    }
+
+    return my_string;
+
+}
